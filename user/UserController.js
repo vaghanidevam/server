afa9641e07255f95e0982c3d00df61010b8581a7
devam vaghani <vaghanidevam11@gmail.com>
2025-05-14 15:44:28 +0530

    first commit

--- user/UserController.js
+++ user/UserController.js
@@ -1,7 +1,7 @@
 const User = require('./user');
 const OTP = require('../otp/otp');
 const otpGenerator = require('otp-generator');
-const becrypt = require("bcrypt");
+const bcrypt = require('bcryptjs'); // or 'bcrypt' if you're using it
 const profile = require('../profile/profile');
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
